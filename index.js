006e30fca2520bfea56acda2c3ee08c650221f71
Sharon-Mosher <64615121+Sharon-Mosher@users.noreply.github.com>
2020-05-30 22:42:06 -0400

    Finished tasks 1-3 in JavaScript

--- index.js
+++ index.js
@@ -229,11 +229,13 @@ const artists = [
 (2) Bio of the third artist (2nd index) in the array */
 
 console.log(artists[0].name);
-
 console.log(artists[2].bio);
 
-/* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+/* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. 
+Use an array method to fix this issue and console.log() to check your work. */
+artists[8].name ="Vincent Van Gogh"
+console.log(artists[8].name);
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
  *     (2) a number which is the desired index in the array.
@@ -251,7 +253,8 @@ console.log(getArtistByIndex(artists, 0));
 /**
 
 
-/* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born the 20th century (1900-2000) */
+/* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of 
+artists who were born the 20th century (1900-2000) */
 
 function get20s(/* Code here */) {
   /* Code here */
@@ -276,7 +279,8 @@ function removeArtist(/*code here*/) {
 
 
 
-/* Task 6: Create a function called `addArtist` that can accept an array of information and add it to the artists array. Then, add a 21st artist to the array (you) with custom information!👩‍🎨👨‍🎨
+/* Task 6: Create a function called `addArtist` that can accept an array of information and add it to the artists
+ array. Then, add a 21st artist to the array (you) with custom information!👩‍🎨👨‍🎨
 
 For example, you could add the following information: 
 id: 21
